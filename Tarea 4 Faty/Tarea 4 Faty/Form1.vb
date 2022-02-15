e33048de7b317f1e94b5ab7f4ae495b130b70c61
fdefati <99689962+fdefati@users.noreply.github.com>
2022-02-15 13:46:20 -0600

    Add files via upload

---
+++ Tarea 4 Faty/Tarea 4 Faty/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num, i, factorial As Double
+        num = CInt(TextBox1.Text)
+        factorial = 1
+        For i = 1 To num
+            factorial = factorial * i
+        Next
+        TextBox2.Text = factorial
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+End Class
